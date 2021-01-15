0c00d1e98f54876ab39d579a8b3801bd9737eed2
AlphaArslan <arslan.alpha21@gmail.com>
2021-01-15 21:53:35 +0200

    Counter value control by push button is functional

--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-# decreament pb on GPIO23 , 36 , D23
-#
-#
-#
+# decreament pb on GPIO23 , D23
+# increament pb on GPIO22 , D22
+# reset pb on GPIO21 , D21
 
 from machine import Pin, Timer
 from time import sleep
 
-def dec_handle(pin):
-    print("|| PRESSED")
+DEC_PIN = 23
+INC_PIN = 22
+RST_PIN = 21
+
+counter_value = 0
+pressed_pb = None
+
+def handler(t):
+    global pressed_pb
+    global counter_value
+    if pressed_pb == Pin(RST_PIN):                     # reset
+        counter_value = 0
+    elif pressed_pb == Pin(DEC_PIN):                   # decreament
+        if counter_value == 0:
+            counter_value = 9
+        else:
+            counter_value = counter_value - 1
+    else:                                       # increment
+        if counter_value == 9:
+            counter_value = 0
+        else:
+            counter_value = counter_value + 1
+    print("||", counter_value)
 
 def debounce(pin):
-    timer.init(mode=Timer.ONE_SHOT, period=200, callback=lambda pin: dec_handle(pin))
+    global pressed_pb
+    pressed_pb = pin
+    timer.init(mode=Timer.ONE_SHOT, period=200, callback=handler)
+
 
 timer = Timer(0)
-dec_pb = Pin(23, Pin.IN , pull=Pin.PULL_UP)
+dec_pb = Pin(DEC_PIN, Pin.IN , pull=Pin.PULL_UP)
 dec_pb.irq(handler=debounce, trigger=Pin.IRQ_FALLING)
-dec_pb.value()
+inc_pb = Pin(INC_PIN, Pin.IN , pull=Pin.PULL_UP)
+inc_pb.irq(handler=debounce, trigger=Pin.IRQ_FALLING)
+
+# temp auxillary
+def reset():
+    import machine
+    machine.reset()
